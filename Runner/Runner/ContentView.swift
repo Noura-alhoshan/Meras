ba64900da0bdd5c23b930837a0ee08ef860f8c4e
haifaHamad <63456009+haifaHamad@users.noreply.github.com>
2021-09-15 01:59:47 +0300

    Add files via upload

---
+++ Runner/Runner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Runner
+//
+//  Created by Haifa Alfrayan on 07/02/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
