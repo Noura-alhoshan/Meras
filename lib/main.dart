50866e52b48776b42811fe4b1d281596e1c111b8
LeenaMulia <hurt139@gmail.com>
2021-09-16 04:36:37 +0300

    Navigation bar with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'ADcategory.dart';
 import 'TRcategory.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MaterialApp(
     home: ADcategory(),
   ));
