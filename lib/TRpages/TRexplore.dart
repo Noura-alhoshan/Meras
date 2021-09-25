3c6a167741d503f191827e38c877aa5764bebb09
LeenaMulia <hurt139@gmail.com>
2021-09-25 20:47:02 +0300

    Updated Local Notification

--- lib/TRpages/TRexplore.dart
+++ lib/TRpages/TRexplore.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import '../NotificationsHandler.dart';
 
 class TRexplore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(
-          onPressed: () async {
-            await createLocalNotification(message: {
-              'title': 'Admin',
-              'message': 'Do you want to Approve/Disapprove this coach?'
-            });
-          },
-          child: Text('Click')),
+      child: Text('صفحة المدربين'),
     );
   }
 }
