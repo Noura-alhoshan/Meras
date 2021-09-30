120d68c3ff43ed837aadf978396d440fa6ab3683
LeenaMulia <hurt139@gmail.com>
2021-10-01 01:23:21 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  //App is terminated
+//App is terminated
   FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
 
-  //App is closed(Inside the RAM)
+//App is closed(Inside the RAM)
   FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
     await handleNetworkNotification(message);
   });
 
-  // App is opened
+// App is opened
   FirebaseMessaging.onMessageOpenedApp.listen((message) async {
     await handleNetworkNotification(message);
   });
