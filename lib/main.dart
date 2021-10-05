16991ae328cf9a0568f10487be79568e7861f571
LeenaMulia <hurt139@gmail.com>
2021-10-05 20:17:40 +0300

    Main Branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:meras_sprint1/TRcategory.dart';
 import 'ADcategory.dart';
+import 'ADpages/ADlist.dart';
 import 'NotificationsHandler.dart';
 
 void main() async {
@@ -21,6 +24,11 @@ void main() async {
   FirebaseMessaging.onMessageOpenedApp.listen((message) async {
     await createLocalNotification(message: message.data);
   });
+
+  //initializeLocalNotification();
+  //AwesomeNotifications().actionStream.listen((receivedNotification) {
+  //Get.to(ADlist());
+  //});
 
   initializeLocalNotification();
 
