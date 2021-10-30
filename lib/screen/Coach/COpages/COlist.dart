b3344bf7e04d4db1a1180c946ae548c3677e27ce
Noura Alhoshan <nooni-4321@hotmail.com>
2021-10-30 21:54:17 +0300

    Add files via upload

--- lib/screen/Coach/COpages/COlist.dart
+++ lib/screen/Coach/COpages/COlist.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+
 import 'package:meras/screen/home/navDrawer.dart';
+
+import '../../../constants.dart';
+import 'requests/PendingRequest.dart';
+import 'requests/acceptedRequest.dart';
+import 'requests/rejectedRequest.dart';
 
 class COlist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('الطلبات'),
-        backgroundColor: Colors.deepPurple[100],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          extendBody: true,
+          //extendBodyBehindAppBar: true,
+          drawer: NavDrawer(),
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurple[100],
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'المرفوضة'),
+                Tab(
+                  text: 'قيد الأنتظار',
+                ),
+                Tab(text: 'المقبولة'),
+              ],
+              indicatorColor: kPrimaryColor,
+              unselectedLabelColor: Colors.white54,
+            ),
+            title: Text('        قائمة طلبات التدريب'),
+          ),
+          body: TabBarView(
+            children: [
+              // Text('المرفوضة'),
+              // Text('المعلقة'),
+              // Text('المقبولة'),
+
+              new RejectedRequest(),
+              new PendingRequest(),
+              new AcceptedRequest(),
+            ],
+          ),
+        ),
       ),
     );
   }
