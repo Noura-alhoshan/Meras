0d629d0efca764930a3e36077411d7a97ca3e75b
Leena Mulia <90129101+LeenaMulia@users.noreply.github.com>
2021-11-20 21:55:03 +0300

    Update manageGuidlines.dart

--- lib/screen/Admin/ADpages/manageGuidlines.dart
+++ lib/screen/Admin/ADpages/manageGuidlines.dart
@@ -3,9 +3,11 @@ import 'package:meras/components/adminRounded_button.dart';
 import 'package:meras/screen/Admin/ADpages/addGuidlines.dart';
 import 'package:meras/screen/Admin/ADpages/editGuidlines.dart';
 import 'package:meras/screen/Admin/widget/BackgroundA.dart';
+import 'package:meras/screen/Trainee/TRpages/TRguidelines.dart';
 import 'package:meras/screen/authenticate/background.dart';
 
 import '../../../constants.dart';
+import 'deleteGuidelines.dart';
 
 class ManageGuidlines extends StatefulWidget {
   @override
@@ -42,6 +44,46 @@ class _ManageGuidlinesState extends State<ManageGuidlines> {
                   child: FlatButton(
                     onPressed: () => {
                       Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => TRguidelines()))
+                    },
+                    padding: EdgeInsets.all(5.0),
+                    child: Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "إشارات السير المضافة   ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Icon(
+                            Icons.check,
+                            color: kPrimaryColor,
+                            size: 35,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                width: 350,
+                height: 90,
+                padding: EdgeInsets.all(10),
+                decoration: new BoxDecoration(
+                  color: Colors.deepPurple[50],
+                  border: Border.all(color: Colors.white, width: 0.0),
+                  borderRadius: new BorderRadius.all(Radius.circular(29.0)),
+                ),
+                child: Center(
+                  child: FlatButton(
+                    onPressed: () => {
+                      Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => AddGuidlines()))
                     },
                     padding: EdgeInsets.all(5.0),
@@ -51,7 +93,7 @@ class _ManageGuidlinesState extends State<ManageGuidlines> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           Text(
-                            "إضافة إشارات سير  ",
+                            "إضافة إشارة سير ",
                             style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 19,
@@ -108,8 +150,49 @@ class _ManageGuidlinesState extends State<ManageGuidlines> {
                   ),
                 ),
               ),
+              SizedBox(height: 20.0),
+              Container(
+                width: 350,
+                height: 90,
+                padding: EdgeInsets.all(10),
+                decoration: new BoxDecoration(
+                  color: Colors.deepPurple[50],
+                  border: Border.all(color: Colors.white, width: 0.0),
+                  borderRadius: new BorderRadius.all(Radius.circular(29.0)),
+                ),
+                child: Center(
+                  child: FlatButton(
+                    onPressed: () => {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => deleteGuidelines()))
+                    },
+                    padding: EdgeInsets.all(5.0),
+                    child: Center(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "حذف إشارة سير  ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 19,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Icon(
+                            Icons.clear,
+                            color: kPrimaryColor,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ));
   }
 }
+
